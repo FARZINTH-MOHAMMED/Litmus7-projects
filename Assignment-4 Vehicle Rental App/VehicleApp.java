a72cc9e0597a42c77c5dfaff346a131b2788b85e
FARZINTH-MOHAMMED <farzinthmohammed.mec@gmail.com>
2025-06-22 12:33:30 +0530

    Create VehicleApp.java

---
+++ Assignment-4 Vehicle Rental App/VehicleApp.java
@@ -0,0 +1,35 @@
+package com.litmus7.vehiclerental;
+
+import com.litmus7.vehiclerental.dto.*;
+
+/**
+ * The {@code VehicleApp} class serves as the entry point of the application. It
+ * creates {@code Car} and {@code Bike} objects, take input values and display
+ * values for these objects
+ * 
+ * @author Farzinth Mohammed
+ */
+public class VehicleApp {
+
+	public static void main(String[] args) {
+		Car car1 = new Car();
+		car1.displayDetails();
+
+		car1.setInputDetails();
+		car1.displayDetails();
+
+		Car car2 = new Car("Toyota", "model-s", 700, 4, true);
+		car2.displayDetails();
+
+		Bike bike1 = new Bike();
+		bike1.displayDetails();
+
+		bike1.setInputDetails();
+		bike1.displayDetails();
+
+		Bike bike2 = new Bike("Kawasaki", "ninja", 400, true, 1200);
+		bike2.displayDetails();
+
+	}
+
+}
