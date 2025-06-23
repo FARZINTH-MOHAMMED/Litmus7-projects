6feb7e4f61e2a0d4a2ed0860a8a3700a1491a7f5
FARZINTH-MOHAMMED <farzinthmohammed.mec@gmail.com>
2025-06-23 15:44:39 +0530

    Update Vehicle.java

--- Assignment-4 Vehicle Rental App/Vehicle.java
+++ Assignment-4 Vehicle Rental App/Vehicle.java
@@ -13,6 +13,30 @@ public class Vehicle {
 	private String model;
 	private double rentalPricePerDay;
 
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public double getRentalPricePerDay() {
+		return rentalPricePerDay;
+	}
+
+	public void setRentalPricePerDay(double rentalPricePerDay) {
+		this.rentalPricePerDay = rentalPricePerDay;
+	}
+
 	/**
 	 * Default constructor for vehicle
 	 */
