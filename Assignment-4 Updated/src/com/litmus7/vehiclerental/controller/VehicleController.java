a262dd6a51850e38354d86ac261055a09dc8bd0d
FARZINTH-MOHAMMED <farzinthmohammed.mec@gmail.com>
2025-07-01 08:09:27 +0530

    Update VehicleController.java

--- Assignment-4 Updated/src/com/litmus7/vehiclerental/controller/VehicleController.java
+++ Assignment-4 Updated/src/com/litmus7/vehiclerental/controller/VehicleController.java
@@ -1,6 +1,5 @@
 package com.litmus7.vehiclerental.controller;
 
-
 import com.litmus7.vehiclerental.dto.Response;
 import com.litmus7.vehiclerental.dto.Vehicle;
 import com.litmus7.vehiclerental.exception.VehicleServiceException;
@@ -14,12 +13,15 @@ public class VehicleController {
 	private VehicleService vehicleService = new VehicleService();
 
 	public Response addVehicle(Vehicle vehicle) {
-		Response response=new Response();
+		Response response = new Response();
 		if (vehicle != null) {
 			vehicleService.addVehicle(vehicle);
+			response.setStatusCode(ERROR_STATUS_CODE);
 			response.setResponseMessage("Vehicle added succesfully");
-		} else
+		} else {
+			response.setStatusCode(SUCCESS_STATUS_CODE);
 			response.setErrorMessage("Couldn't add vehicle");
+		}
 		return response;
 	}
 
@@ -39,54 +41,49 @@ public class VehicleController {
 	}
 
 	public Response rentVehicle(Vehicle vehicle) {
-		Response response= new Response();
-		if(vehicle!=null) {
+		Response response = new Response();
+		if (vehicle != null) {
 			try {
 				response.setVehicles(vehicleService.rentVehicle(vehicle));
 				response.setStatusCode(SUCCESS_STATUS_CODE);
 				response.setResponseMessage("Vehicle rented succesfully");
-				
+
 			} catch (VehicleServiceException e) {
 				response.setStatusCode(ERROR_STATUS_CODE);
 				response.setErrorMessage(e.getMessage());
 			}
-		}
-		else {
+		} else {
 			return null;
 		}
 		return response;
 	}
-	
+
 	public Vehicle searchVehicleByBrand(String brand) {
-		if(brand!=null) {
+		if (brand != null) {
 			return vehicleService.searchVehicleByBrand(brand);
-		}
-		else return null;
+		} else
+			return null;
 	}
-	
-	
+
 	public Vehicle searchVehicleByModel(String model) {
-		if(model!=null) {
+		if (model != null) {
 			return vehicleService.searchVehicleByModel(model);
-		}
-		else return null;
+		} else
+			return null;
 	}
-	
+
 	public double calculateTotalRentalPrice() {
 		return vehicleService.calculateTotalRentalPrice();
 	}
-	
-	
 
 	public String returnVehicle(Vehicle vehicle) {
-		if(vehicle!=null) {		
+		if (vehicle != null) {
 			try {
 				return vehicleService.returnVehicle(vehicle);
 			} catch (VehicleServiceException e) {
 				return e.getMessage();
 			}
-		}
-		else {
+		} else {
 			return null;
 		}
 	}
