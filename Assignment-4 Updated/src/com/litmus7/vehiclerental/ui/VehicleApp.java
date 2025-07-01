18cb052d2bfff4430ee1aa7c4abf241084ab1159
FARZINTH-MOHAMMED <farzinthmohammed.mec@gmail.com>
2025-07-01 08:13:15 +0530

    Update VehicleApp.java

--- Assignment-4 Updated/src/com/litmus7/vehiclerental/ui/VehicleApp.java
+++ Assignment-4 Updated/src/com/litmus7/vehiclerental/ui/VehicleApp.java
@@ -21,52 +21,43 @@ public class VehicleApp {
 
 	public static void main(String[] args) {
 		VehicleController vehicleController = new VehicleController();
-		Vehicle car1= new Car("Hummer","v6",500,6,true);
-		
-		List<Vehicle> vehicles=new ArrayList<>();
-	
-		Response loadVehicleResponse= vehicleController.loadVehicleList("vehicle.txt");
-		if(loadVehicleResponse.getStatusCode()==200) {
+		Vehicle car1 = new Car("Hummer", "v6", 500, 6, true);
+
+		List<Vehicle> vehicles = new ArrayList<>();
+
+		Response loadVehicleResponse = vehicleController.loadVehicleList("vehicle.txt");
+		if (loadVehicleResponse.getStatusCode() == 200) {
 			System.out.println(loadVehicleResponse.getVehicles());
-		}
-		else {
+		} else {
 			System.out.println(loadVehicleResponse.getErrorMessage());
 		}
-		
-		
-		Response addVehicleResponse= vehicleController.addVehicle(car1);
-		if(addVehicleResponse.getStatusCode()==200) {
+
+		Response addVehicleResponse = vehicleController.addVehicle(car1);
+		if (addVehicleResponse.getStatusCode() == 200) {
 			System.out.println(addVehicleResponse.getResponseMessage());
-		}
-		else {
+		} else {
 			System.out.println(addVehicleResponse.getErrorMessage());
 		}
-		
-		
-		Vehicle searchVehicleResponse= vehicleController.searchVehicleByBrand("Toyota");
+
+		Vehicle searchVehicleResponse = vehicleController.searchVehicleByBrand("Toyota");
 		System.out.println(searchVehicleResponse);
-		
-		Vehicle searVehicleByModelRepsonse= vehicleController.searchVehicleByModel("R15");
+
+		Vehicle searVehicleByModelRepsonse = vehicleController.searchVehicleByModel("R15");
 		System.out.println(searVehicleByModelRepsonse);
-		
-		Response rentResponse= vehicleController.rentVehicle(car1);
-		if(rentResponse.getStatusCode()==200) {
+
+		Response rentResponse = vehicleController.rentVehicle(car1);
+		if (rentResponse.getStatusCode() == 200) {
 			System.out.println(rentResponse.getResponseMessage());
-		}
-		else {
+		} else {
 			System.out.println(rentResponse.getErrorMessage());
 		}
-		
-		String returnVehicleResponse= vehicleController.returnVehicle(car1);
+
+		String returnVehicleResponse = vehicleController.returnVehicle(car1);
 		System.out.println(returnVehicleResponse);
-		
-		
-		double totalRentalPrice=vehicleController.calculateTotalRentalPrice();
+
+		double totalRentalPrice = vehicleController.calculateTotalRentalPrice();
 		System.out.println(totalRentalPrice);
-		
-		
-		
-		
+
 	}
 
 }
