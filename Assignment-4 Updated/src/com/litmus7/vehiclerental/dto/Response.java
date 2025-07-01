3c6ad39af0770fdcb8b52cdbeb3fb8e517745402
FARZINTH-MOHAMMED <farzinthmohammed.mec@gmail.com>
2025-07-01 08:11:17 +0530

    Update Response.java

--- Assignment-4 Updated/src/com/litmus7/vehiclerental/dto/Response.java
+++ Assignment-4 Updated/src/com/litmus7/vehiclerental/dto/Response.java
@@ -7,32 +7,37 @@ public class Response {
 	private String errorMessage;
 	private String responseMessage;
 	private List<Vehicle> vehicles;
-	
-	
+
 	public String getResponseMessage() {
 		return responseMessage;
 	}
+
 	public void setResponseMessage(String responseMessage) {
 		this.responseMessage = responseMessage;
 	}
+
 	public int getStatusCode() {
 		return statusCode;
 	}
+
 	public void setStatusCode(int statusCode) {
 		this.statusCode = statusCode;
 	}
+
 	public String getErrorMessage() {
 		return errorMessage;
 	}
+
 	public void setErrorMessage(String errorMessage) {
 		this.errorMessage = errorMessage;
 	}
+
 	public List<Vehicle> getVehicles() {
 		return vehicles;
 	}
+
 	public void setVehicles(List<Vehicle> vehicles) {
 		this.vehicles = vehicles;
 	}
-	
-	
+
 }
