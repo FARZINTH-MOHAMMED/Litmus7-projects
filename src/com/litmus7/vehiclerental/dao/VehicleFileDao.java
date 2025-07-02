1ea28f9407b15db9ae007b262824c146997103bc
FARZINTH-MOHAMMED <farzinthmohammed.mec@gmail.com>
2025-07-02 19:22:56 +0530

    Add files via upload

---
+++ src/com/litmus7/vehiclerental/dao/VehicleFileDao.java
@@ -0,0 +1,55 @@
+package com.litmus7.vehiclerental.dao;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.litmus7.vehiclerental.dto.Bike;
+import com.litmus7.vehiclerental.dto.Car;
+import com.litmus7.vehiclerental.dto.Vehicle;
+import com.litmus7.vehiclerental.exception.VehicleDataAccessExecption;
+
+public class VehicleFileDao {
+
+	public List<Vehicle> loadVehicle(String filepath) throws VehicleDataAccessExecption {
+
+		List<Vehicle> vehicles = new ArrayList<>();
+
+		try (BufferedReader br = new BufferedReader(new FileReader(filepath))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				Vehicle vehicle = parseData(line);
+				vehicles.add(vehicle);
+			}
+
+		} catch (IOException e) {
+			throw new VehicleDataAccessExecption("Data couldn't loaded");
+		} catch (Exception e) {
+			throw new VehicleDataAccessExecption("Some error occured");
+		}
+		return vehicles;
+	}
+
+	private Vehicle parseData(String line) {
+		String[] parts = line.split(",");
+		String type = parts[0].trim();
+		if (parts.length == 6) {
+			String brand = parts[1].trim();
+			String model = parts[2].trim();
+			double rentalPricePerDay = Double.parseDouble(parts[3].trim());
+			if (type.equals("Car")) {
+				int numberOfDoors = Integer.parseInt(parts[4].trim());
+				boolean isAutomatic = Boolean.parseBoolean(parts[5].trim());
+				return new Car(brand, model, rentalPricePerDay, numberOfDoors, isAutomatic);
+			} else if (type.equals("Bike")) {
+				boolean hasGear = Boolean.parseBoolean(parts[4].trim());
+				int engineCapacity = Integer.parseInt(parts[5].trim());
+				return new Bike(brand, model, rentalPricePerDay, hasGear, engineCapacity);
+			}
+		}
+		return null;
+	}
+
+}
---
+++ src/com/litmus7/vehiclerental/dto/Bike.java
@@ -0,0 +1,55 @@
+package com.litmus7.vehiclerental.dto;
+
+import java.util.Scanner;
+
+/**
+ * Bike class inheriting {@code Vehicle} class including bike specifc
+ * attributes.
+ * 
+ * @author Farzinth Mohammed
+ */
+public class Bike extends Vehicle {
+	private boolean hasGear;
+	private int engineCapacity;
+
+	/**
+	 * Default constructor
+	 */
+	public Bike() {
+		hasGear = false;
+		engineCapacity = 0;
+	}
+
+	public boolean hasGear() {
+		return hasGear;
+	}
+
+	public void setHasGear(boolean hasGear) {
+		this.hasGear = hasGear;
+	}
+
+	public int getEngineCapacity() {
+		return engineCapacity;
+	}
+
+	public void setEngineCapacity(int engineCapacity) {
+		this.engineCapacity = engineCapacity;
+	}
+
+	/**
+	 * Parameterized constructor with invoking {@code Vehicle} class parameterized
+	 * constructor.
+	 * 
+	 * @param brand
+	 * @param model
+	 * @param rentalPricePerDay
+	 * @param hasGear
+	 * @param engineCapacity
+	 */
+	public Bike(String brand, String model, double rentalPricePerDay, boolean hasGear, int engineCapacity) {
+		super(brand, model, rentalPricePerDay);
+		this.hasGear = hasGear;
+		this.engineCapacity = engineCapacity;
+	}
+
+}
